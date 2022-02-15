ba8f9e24625275341bdc307a934ae2de113e866e
Adhiraj Singh <adhirajsingh1001@gmail.com>
2022-02-15 17:25:06 +0530

    fix: incorrectly map recipient

--- src/transfer-mapping.ts
+++ src/transfer-mapping.ts
@@ -2,5 +2,5 @@ import { Transfer } from '../generated/GrantTransfersDAI/ERC20'
 import { applyGrantFundUpdate } from './utils/apply-grant-deposit'
 
 export function handleTransfer(event: Transfer): void {
-	applyGrantFundUpdate(event, true, event.params.to.toHex(), event.params.value, event.params.from, event.block.timestamp.toI32())
+	applyGrantFundUpdate(event, true, event.params.to.toHex(), event.params.value, event.params.to, event.block.timestamp.toI32())
 }
