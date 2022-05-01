679558afb4704c2549e190dd72df8e453b18a09f
Sweta Shaw <swt.shw94@gmail.com>
2022-05-01 23:36:56 +0530

    chore: remove set entity from workspace update

--- src/utils/generics.ts
+++ src/utils/generics.ts
@@ -17,7 +17,7 @@ export function mapGrantFieldMap(grantId: string, map: GrantFieldMap): string[] 
 	fields.push(mapGrantField(grantId, 'fundingBreakdown', map.fundingBreakdown))
 
 	const additionalEntries = map.additionalProperties.entries
-	for(let i = 0;i < additionalEntries.length;i++) {
+	for (let i = 0; i < additionalEntries.length; i++) {
 		fields.push(mapGrantField(grantId, additionalEntries[i].key, additionalEntries[i].value))
 	}
 
@@ -27,7 +27,7 @@ export function mapGrantFieldMap(grantId: string, map: GrantFieldMap): string[] 
 export function mapGrantFieldAnswers(applicationId: string, grantId: string, map: GrantApplicationFieldAnswers): string[] {
 	const fields: string[] = []
 	const additionalEntries = map.additionalProperties.entries
-	for(let i = 0;i < additionalEntries.length;i++) {
+	for (let i = 0; i < additionalEntries.length; i++) {
 		fields.push(mapGrantFieldAnswer(applicationId, grantId, additionalEntries[i].key, additionalEntries[i].value))
 	}
 
@@ -38,7 +38,7 @@ export function mapGrantPII(applicationId: string, grantId: string, map: PIIAnsw
 	const items: string[] = []
 
 	const entryList = map.additionalProperties.entries
-	for(let i = 0;i < entryList.length;i++) {
+	for (let i = 0; i < entryList.length; i++) {
 		const entry = entryList[i]
 		const item = new PIIAnswer(`${applicationId}.${entry.key}`)
 		item.manager = `${grantId}.${entry.key}`
@@ -53,7 +53,7 @@ export function mapGrantPII(applicationId: string, grantId: string, map: PIIAnsw
 
 export function mapMilestones(applicationId: string, milestoneList: GrantProposedMilestone[]): string[] {
 	const milestones: string[] = []
-	for(let i = 0;i < milestoneList.length;i++) {
+	for (let i = 0; i < milestoneList.length; i++) {
 		const milestone = new ApplicationMilestone(`${applicationId}.${i}`)
 		milestone.state = 'submitted'
 		milestone.title = milestoneList[i].title
@@ -69,22 +69,22 @@ export function mapMilestones(applicationId: string, milestoneList: GrantPropose
 
 export function contractApplicationStateToString(state: i32): Result<string> {
 	let strState: string
-	switch(state) {
+	switch (state) {
 		case 0:
 			strState = 'submitted'
-		break
+			break
 		case 1:
 			strState = 'resubmit'
-		break
+			break
 		case 2:
 			strState = 'approved'
-		break
+			break
 		case 3:
 			strState = 'rejected'
-		break
+			break
 		case 4:
 			strState = 'completed'
-		break
+			break
 		default:
 			return { value: null, error: `Unknown app state "${state}"` }
 	}
@@ -94,16 +94,16 @@ export function contractApplicationStateToString(state: i32): Result<string> {
 
 export function contractMilestoneStateToString(state: i32): Result<string> {
 	let stateStr: string
-	switch(state) {
+	switch (state) {
 		case 0:
 			stateStr = "submitted"
-		break
+			break
 		case 1:
 			stateStr = "requested"
-		break
+			break
 		case 2:
 			stateStr = "approved"
-		break
+			break
 		default:
 			return { value: null, error: `Unknown milestone state "${state}"` }
 	}
@@ -113,7 +113,7 @@ export function contractMilestoneStateToString(state: i32): Result<string> {
 
 export function mapWorkspaceSupportedNetworks(networksList: string[]): string[] {
 	const items: string[] = []
-	for(let i = 0;i < networksList.length;i++) {
+	for (let i = 0; i < networksList.length; i++) {
 		items.push(`chain_${networksList[i]}`)
 	}
 	return items
@@ -121,12 +121,12 @@ export function mapWorkspaceSupportedNetworks(networksList: string[]): string[] 
 
 export function mapWorkspaceSocials(workspaceId: string, socialsList: SocialItem[]): string[] {
 	const items: string[] = []
-	for(let i = 0;i < socialsList.length;i++) {
+	for (let i = 0; i < socialsList.length; i++) {
 		const social = new Social(`${workspaceId}.${socialsList[i].name}`)
 		social.name = socialsList[i].name
 		social.value = socialsList[i].value
 		social.save()
-		
+
 		items.push(social.id)
 	}
 	return items
@@ -140,6 +140,7 @@ export function mapWorkspaceTokens(workspaceId: string, tokensList: TokenItem[])
 		token.address = tokensList[i].address
 		token.decimal = tokensList[i].decimal.toI32()
 		token.iconHash = tokensList[i].iconHash
+		token.workspace = workspaceId
 		token.save()
 
 		items.push(token.id)
@@ -149,13 +150,13 @@ export function mapWorkspaceTokens(workspaceId: string, tokensList: TokenItem[])
 
 export function mapGrantManagers(managerWalletIds: Bytes[] | null, grantId: string, workspaceId: string): string[] {
 	const items: string[] = []
-	if(managerWalletIds) {
-		for(let i = 0;i < managerWalletIds.length;i++) {
+	if (managerWalletIds) {
+		for (let i = 0; i < managerWalletIds.length; i++) {
 			const walletId = managerWalletIds[i].toHex()
 			const manager = new GrantManager(`${grantId}.${walletId}`)
 			manager.grant = grantId
 			manager.member = `${workspaceId}.${walletId}`
-			
+
 			manager.save()
 			items.push(manager.id)
 		}
@@ -164,14 +165,14 @@ export function mapGrantManagers(managerWalletIds: Bytes[] | null, grantId: stri
 }
 
 export function removeEntityCollection(entityName: string, ids: string[]): void {
-	for(let i = 0;i < ids.length;i++) {
+	for (let i = 0; i < ids.length; i++) {
 		store.remove(entityName, ids[i])
 	}
 }
 
 function mapGrantFieldAnswer(applicationId: string, grantId: string, title: string, answers: GrantApplicationFieldAnswerItem[]): string {
 	const answer = new GrantFieldAnswer(`${applicationId}.${title}`)
-	for(let i = 0;i < answers.length;i++) {
+	for (let i = 0; i < answers.length; i++) {
 		const ansValue = new GrantFieldAnswerItem(`${answer.id}.${i}`)
 		ansValue.answer = answer.id
 		ansValue.value = answers[i].value
@@ -179,7 +180,7 @@ function mapGrantFieldAnswer(applicationId: string, grantId: string, title: stri
 		ansValue.save()
 	}
 	answer.field = `${grantId}.${title}`
-	
+
 	answer.save()
 
 	return answer.id
@@ -200,15 +201,16 @@ export function mapGrantRewardAndListen(id: string, workspaceId: string, rewardJ
 	const reward = new Reward(id)
 	reward.asset = rewardJson.asset
 	reward.committed = rewardJson.committed
-	if(rewardJson.token) {
-		reward.token = rewardJson.token.address.toHex()
+	if (rewardJson.token) {
+		const token = mapWorkspaceTokens(workspaceId, [rewardJson.token])
+		reward.token = token[0]
 	}
 	reward.save()
 
 	const hexAssetAddr = reward.asset.toHex()
 
 	GrantTransfersERC20.create(
-	  Address.fromString(hexAssetAddr)
+		Address.fromString(hexAssetAddr)
 	)
 
 	log.info(`listening to ERC20 "${hexAssetAddr}"`, [])
