388df3c30d3aff05b6f690027e77446418cd71f8
Richik Chanda <richikchanda1999@gmail.com>
2022-10-12 19:12:37 +0530

    fix: changed all === to ==

--- src/utils/notifications.ts
+++ src/utils/notifications.ts
@@ -7,7 +7,7 @@ export function addFundsTransferNotification(transfer: FundsTransfer): void {
 		const workspace = Workspace.load(grant.workspace)
 		if(workspace) {
 			const notif = new Notification(`n.${transfer.id}`)
-			if(transfer.type === 'funds_disburse') {
+			if(transfer.type == 'funds_disburse') {
 				notif.title = 'Funds Released!'
 				notif.content = `'${workspace.title}' just released ${transfer.amount.toString()} to your wallet for your application to '${grant.title}'`
 
@@ -19,13 +19,13 @@ export function addFundsTransferNotification(transfer: FundsTransfer): void {
 
 				notif.recipientIds = [app.applicantId]
 				notif.entityId = app.id
-			} else if(transfer.type === 'funds_deposited') {
+			} else if(transfer.type == 'funds_deposited') {
 				notif.title = 'Funds Deposited!'
 				notif.content = ''
 
 				notif.recipientIds = [grant.creatorId]
 				notif.entityId = grant.id
-			} else if(transfer.type === 'funds_withdrawn') {
+			} else if(transfer.type == 'funds_withdrawn') {
 				notif.title = 'Funds Withdrawn!'
 				notif.content = ''
 
@@ -54,13 +54,13 @@ export function addApplicationUpdateNotification(application: GrantApplication, 
 		}
 
 		const notif = new Notification(`n.${eventId}`)
-		if(application.state === 'submitted') {
+		if(application.state == 'submitted') {
 			notif.title = 'New Application Submitted!'
 			notif.content = `${application.applicantId.toHex()} just submitted an application for your grant '${grant.title}'`
 			notif.type = 'application_submitted'
 
 			notif.recipientIds = [grant.creatorId]
-		} else if(application.state === 'resubmit') {
+		} else if(application.state == 'resubmit') {
 			notif.title = 'Application Resubmission Required'
 			const workspace = Workspace.load(grant.workspace)
 			if(!workspace) {
@@ -72,19 +72,19 @@ export function addApplicationUpdateNotification(application: GrantApplication, 
 			notif.type = 'application_resubmitted'
 
 			notif.recipientIds = [application.applicantId]
-		} else if(application.state === 'approved') {
+		} else if(application.state == 'approved') {
 			notif.title = 'Application Approved!'
 			notif.content = `Your application to '${grant.title}' was just approved. Congratulations!`
 			notif.type = 'application_accepted'
 
 			notif.recipientIds = [application.applicantId]
-		} else if(application.state === 'rejected') {
+		} else if(application.state == 'rejected') {
 			notif.title = 'Application Rejected'
 			notif.content = `We regret to inform you that your application to '${grant.title}' was rejected.\n\n--\n\n${feedbackDao!}`
 			notif.type = 'application_rejected'
 
 			notif.recipientIds = [application.applicantId]
-		} else if(application.state === 'completed') {
+		} else if(application.state == 'completed') {
 			notif.title = 'Application Completed'
 			notif.content = `Your application to '${grant.title}' was marked completed. Congratulations!`
 			notif.type = 'application_completed'
@@ -112,7 +112,7 @@ export function addMilestoneUpdateNotification(milestone: ApplicationMilestone, 
 			const workspace = Workspace.load(grant.workspace)
 			if(workspace) {
 				const notif = new Notification(`n.${eventId}`)
-				if(milestone.state === 'submitted') {
+				if(milestone.state == 'submitted') {
 					notif.title = 'Milestone Request Rejected'
 					let text = milestone.feedbackDao
 					if(!text) {
@@ -123,13 +123,13 @@ export function addMilestoneUpdateNotification(milestone: ApplicationMilestone, 
 					notif.type = 'milestone_rejected'
 
 					notif.recipientIds = [application.applicantId]
-				} else if(milestone.state === 'requested') {
+				} else if(milestone.state == 'requested') {
 					notif.title = 'Milestone Fund Release Requested'
 					notif.content = `${application.applicantId.toHex()} has requested release of their payment for a milestone on their application to ${grant.title}`
 					notif.type = 'milestone_requested'
 
 					notif.recipientIds = [grant.creatorId]
-				} else if(milestone.state === 'approved') {
+				} else if(milestone.state == 'approved') {
 					notif.title = 'Milestone Approved!'
 					notif.content = `${workspace.title} has approved your milestone on your application to their grant ${grant.title}`
 					notif.type = 'milestone_accepted'
