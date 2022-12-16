3c0cb79e7d34de08e82b88dff0f9efbfff8ab58a
Sweta Shaw <swt.shw94@gmail.com>
2022-12-16 17:24:07 +0530

    feat: add comment handler

---
+++ src/communication-mapping.ts
@@ -0,0 +1,54 @@
+import { log } from '@graphprotocol/graph-ts'
+import { CommentAdded } from '../generated/QBCommunicationContract/QBCommunicationContract'
+import { Comment, GrantApplication, PIIData } from '../generated/schema'
+import { validatedJsonFromIpfs } from './json-schema/json'
+import { PrivateCommentAddRequest, validatePrivateCommentAddRequest } from './json-schema'
+
+export function handleCommentAdded(event: CommentAdded): void {
+	const workspaceId = event.params.workspaceId.toHex()
+	const  grantAddress = event.params.grantAddress.toHex()
+	const applicationId = event.params.applicationId.toHex()
+	const isPrivate = event.params.isPrivate
+	const commentMetadataHash = event.params.commentMetadataHash
+	const sender = event.params.sender.toHex()
+	const timestamp = event.params.timestamp.toI32()
+
+	const application = GrantApplication.load(applicationId)
+	if(!application) {
+		log.warning(`[${event.transaction.hash}] Application with ID "${applicationId}" not found`, [])
+		return
+	}
+
+	const commentEntity = new Comment(`${event.transaction.hash.toHex()}-${sender}`)
+	
+	commentEntity.workspace = workspaceId
+	commentEntity.grant = grantAddress
+	commentEntity.application = applicationId
+	commentEntity.isPrivate = isPrivate
+	commentEntity.createdAt = timestamp
+
+	if(isPrivate) {
+		const result  = validatedJsonFromIpfs<PrivateCommentAddRequest>(commentMetadataHash, validatePrivateCommentAddRequest)
+		if(result) {
+			const encryptedCommentPiiDataList = result.value!.pii.additionalProperties.entries
+			const items: string[] = []
+			if(encryptedCommentPiiDataList) {
+				log.info(`[${event.transaction.hash.toHex()}] Found {} encrypted comment PII data`, [encryptedCommentPiiDataList.length.toString()])
+				for(let i=0; i<encryptedCommentPiiDataList.length; i++) {
+					const encryptedCommentEntity = new PIIData(`${event.transaction.hash.toHex()}-${encryptedCommentPiiDataList[i].key}`)
+
+					encryptedCommentEntity.data = encryptedCommentPiiDataList[i].value
+					encryptedCommentEntity.save()
+					items.push(encryptedCommentEntity.id)
+				}
+
+				commentEntity.commentsEncryptedData = items
+			}
+		}
+	} else {
+		commentEntity.commentsPublicHash = commentMetadataHash
+	}
+
+	commentEntity.save()
+	log.info(`[${event.transaction.hash.toHex()}] [${isPrivate? 'PIVATE': 'PUBLIC'}] Comment added to application ${applicationId} by ${sender}`, [])
+}
